f60e949f9edb2af7369a53383fa0c0e1a3ffaa62
SexyDoGyun <kimdk0903@naver.com>
2025-06-05 17:09:45 +0900

    레벨 3-1: 테스트 코드 연습 - 1

--- src/test/java/org/example/expert/config/PasswordEncoderTest.java
+++ src/test/java/org/example/expert/config/PasswordEncoderTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(SpringExtension.class)
@@ -20,9 +21,18 @@ class PasswordEncoderTest {
         String encodedPassword = passwordEncoder.encode(rawPassword);
 
         // when
-        boolean matches = passwordEncoder.matches(encodedPassword, rawPassword);
+        // passEncoder 클래스의 matches 메서드를 확인해보면 매개변수의 순서가 matches(rawPassword, encodedPassword) 이다
+        // 이때 boolean matches는 참이 된다
+        boolean matches = passwordEncoder.matches(rawPassword, encodedPassword);
+
+        // 만약 매개변수의 순서를 기존에 있는거로 하고싶다면 아래 검증식을 바꿔야한다
+//        boolean matches = passwordEncoder.matches(encodedPassword, rawPassword);
 
         // then
+        // assertTrue는 괄호안의 값이 참이면 테스트를 성공한다
         assertTrue(matches);
+
+        // assertFalse는 괄호안의 식이 false이면 테스트를 성공한다
+//        assertFalse(matches);
     }
 }
